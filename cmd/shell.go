a6ba40d20d17bca8737ca7071e2bd5c8e78313c9
Avital Tamir <avital.tamir@lemonade.com>
2023-11-14 13:58:04 +0200

    Add basic interactive shell

--- cmd/shell.go
+++ cmd/shell.go
@@ -1,24 +1,84 @@
 package cmd
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 
-	"github.com/spf13/cobra"
+	cobra "github.com/spf13/cobra"
 )
 
-// shellCmd represents the shell command
-var shellCmd = &cobra.Command{
+// ShellCommand is the Cobra command for the interactive shell
+var ShellCmd = &cobra.Command{
 	Use:   "shell",
-	Short: "Launch the Cyphernetes interactive shell",
-	Long:  `Use the 'shell' subcommand to enter an interactive shell for executing multiple Cypher-inspired queries against your Kubernetes resources.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// This is where you would initialize and run your interactive shell
-		fmt.Println("Entering interactive shell...")
-	},
+	Short: "Launch an interactive shell",
+	Run:   runShell,
+}
+
+func runShell(cmd *cobra.Command, args []string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Cyphernetes Interactive Shell")
+	fmt.Println("Type 'exit' or press Ctrl-C to exit")
+
+	for {
+		fmt.Print("> ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			// Handle the error according to your needs
+			break
+		}
+
+		input = strings.TrimSpace(input)
+		if input == "exit" {
+			break
+		}
+
+		// Process the input
+		result, err := processQuery(input)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+		} else {
+			fmt.Println(result)
+		}
+	}
+}
+
+func processQuery(query string) (string, error) {
+	// Parse the query to get an AST.
+	ast, err := ParseQuery(query)
+	if err != nil {
+		// Handle error.
+		fmt.Println("Error parsing query: ", err)
+		return "", err
+	}
+
+	// Execute the query against the Kubernetes API.
+	executor, err := NewQueryExecutor()
+	if err != nil {
+		// Handle error.
+		fmt.Println("Error creating query executor: ", err)
+		return "", err
+	}
+	results, err := executor.Execute(ast)
+	if err != nil {
+		// Handle error.
+		fmt.Println("Error executing query: ", err)
+		return "", err
+	}
+	// Print the results as pretty JSON.
+	json, err := json.MarshalIndent(results, "", "  ")
+	if err != nil {
+		// Handle error.
+		fmt.Println("Error marshalling results: ", err)
+		return "", err
+	}
+	return string(json), nil
 }
 
 func init() {
-	rootCmd.AddCommand(shellCmd)
+	rootCmd.AddCommand(ShellCmd)
 
 	// Here you can define flags and configuration settings for the 'shell' subcommand if needed
 }
