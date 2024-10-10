73b21995c87e4720f5b5e91d706ce138859cef6d
James Kim <kgh475926@gmail.com>
2024-10-09 23:22:36 -0400

    reset parser.AllNamespaces to false after it has been set

--- cmd/cyphernetes/shell.go
+++ cmd/cyphernetes/shell.go
@@ -205,6 +205,7 @@ type Listener interface {
 func runShell(cmd *cobra.Command, args []string) {
 	if parser.AllNamespaces {
 		parser.Namespace = ""
+		parser.AllNamespaces = false
 	}
 
 	historyFile := os.Getenv("HOME") + "/.cyphernetes/history"
