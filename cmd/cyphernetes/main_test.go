26e420a89a9199f32d01d39b167fa7693657d77a
Naor Peled <me@naor.dev>
2024-11-06 09:17:30 +0200

    tests pt.2

--- cmd/cyphernetes/main_test.go
+++ cmd/cyphernetes/main_test.go
@@ -93,8 +93,7 @@ Global Flags:
   -A, --all-namespaces     Query all namespaces
   -l, --loglevel string    The log level to use (debug, info, warn, error, fatal, panic) (default "info")
   -n, --namespace string   The namespace to query against (default "default")
-      --no-color           Turn off color formatting
-  `
+      --no-color           Turn off color formatting`
 	checkOutput(t, stdout, expectedContent, "\"cyphernetes shell -h\"")
 }
 
