10cfc7f804945051de55aad8a62e364e76d5b292
Naor Peled <thehecticbyte@gmail.com>
2024-11-09 00:09:21 +0200

    refine raw-output desc

--- cmd/cyphernetes/query.go
+++ cmd/cyphernetes/query.go
@@ -69,5 +69,5 @@ func runQuery(args []string, w io.Writer) {
 
 func init() {
 	rootCmd.AddCommand(queryCmd)
-	queryCmd.PersistentFlags().BoolVarP(&returnRawJsonOutput, "raw-output", "r", false, "Disable colorized JSON output")
+	queryCmd.PersistentFlags().BoolVarP(&returnRawJsonOutput, "raw-output", "r", false, "Disable JSON output formatting")
 }
