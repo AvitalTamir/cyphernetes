48f681ff94298c246efcc8e20b3148bda0fb47ba
Naor Peled <thehecticbyte@gmail.com>
2024-12-17 21:28:56 +0200

    feat(cli): add version

--- cmd/cyphernetes/root.go
+++ cmd/cyphernetes/root.go
@@ -4,43 +4,40 @@ Copyright © 2023 Avital Tamir <avital.osog@gmail.com>
 package main
 
 import (
+	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/avitaltamir/cyphernetes/pkg/parser"
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
+var (
+	Version = "dev"
+)
+
+func getVersionInfo() string {
+	return fmt.Sprintf("Version: %s\nGo Version: %s\n", Version, runtime.Version())
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "cyphernetes",
 	Short: "Cyphernetes is a tool for querying Kubernetes resources",
 	Long:  `Cyphernetes allows you to query Kubernetes resources using a Cypher-like query language.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		versionFlag, _ := cmd.Flags().GetBool("version")
+		if versionFlag {
+			fmt.Print(getVersionInfo())
+			os.Exit(0)
+		}
+		cmd.Help()
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
-}
-
-// TestExecute is a helper function for testing the Execute function
-func TestExecute(args []string) error {
-	// Save the original os.Args
-	oldArgs := os.Args
-	defer func() { os.Args = oldArgs }()
-
-	// Set up the new os.Args for testing
-	os.Args = append([]string{"cmd"}, args...)
-
-	// Create a new root command for testing
-	cmd := &cobra.Command{Use: "test"}
-	cmd.AddCommand(rootCmd)
-
-	// Execute the command
-	return cmd.Execute()
 }
 
 func init() {
@@ -48,7 +45,16 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&parser.LogLevel, "loglevel", "l", "info", "The log level to use (debug, info, warn, error, fatal, panic)")
 	rootCmd.PersistentFlags().BoolVarP(&parser.AllNamespaces, "all-namespaces", "A", false, "Query all namespaces")
 	rootCmd.PersistentFlags().BoolVar(&parser.NoColor, "no-color", false, "Disable colored output in shell and query results")
+	rootCmd.Flags().BoolP("version", "v", false, "Show version and exit")
+	rootCmd.MarkFlagsMutuallyExclusive("version", "v")
 
-	// Add the web command
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "Show version information",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Print(getVersionInfo())
+		},
+	})
+
 	rootCmd.AddCommand(WebCmd)
 }
