08f5281218d73ad9c41473bb1c18219f58087b34
Naor Peled <thehecticbyte@gmail.com>
2024-12-17 21:35:41 +0200

    .

--- cmd/cyphernetes/root.go
+++ cmd/cyphernetes/root.go
@@ -34,8 +34,11 @@ var rootCmd = &cobra.Command{
 	},
 }
 
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+	err := rootCmd.Execute()
+	if err != nil {
 		os.Exit(1)
 	}
 }
