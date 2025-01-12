f66e36943baee95f416a9b14a90a96cdc2c0347d
Avital Tamir <avital.tamir@develeap.com>
2025-01-12 15:10:56 +0200

    Add delete test to e2e

--- pkg/core/e2e/e2e_test.go
+++ pkg/core/e2e/e2e_test.go
@@ -1130,4 +1130,74 @@ var _ = Describe("Cyphernetes E2E", func() {
 			Expect(k8sClient.Delete(ctx, testDeployment)).Should(Succeed())
 		})
 	})
+
+	It("Should delete a deployment correctly", func() {
+		By("Creating test resources")
+		testDeployment := &appsv1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-deployment-12",
+				Namespace: testNamespace,
+				Labels: map[string]string{
+					"app": "test-delete",
+				},
+			},
+			Spec: appsv1.DeploymentSpec{
+				Replicas: ptr.To(int32(1)),
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"app": "test-delete",
+					},
+				},
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{
+							"app": "test-delete",
+						},
+					},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name:  "nginx",
+								Image: "nginx:1.19",
+							},
+						},
+					},
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, testDeployment)).Should(Succeed())
+
+		By("Waiting for deployment to be created")
+		Eventually(func() error {
+			return k8sClient.Get(ctx, client.ObjectKey{
+				Namespace: testNamespace,
+				Name:      "test-deployment-12",
+			}, &appsv1.Deployment{})
+		}, timeout, interval).Should(Succeed())
+
+		By("Executing delete query")
+		provider, err := apiserver.NewAPIServerProvider()
+		Expect(err).NotTo(HaveOccurred())
+
+		executor, err := core.NewQueryExecutor(provider)
+		Expect(err).NotTo(HaveOccurred())
+
+		ast, err := core.ParseQuery(`
+			MATCH (d:Deployment)
+			WHERE d.metadata.name = "test-deployment-12"
+			DELETE d
+		`)
+		Expect(err).NotTo(HaveOccurred())
+
+		_, err = executor.Execute(ast, testNamespace)
+		Expect(err).NotTo(HaveOccurred())
+
+		By("Verifying the deployment was deleted")
+		Eventually(func() error {
+			return k8sClient.Get(ctx, client.ObjectKey{
+				Namespace: testNamespace,
+				Name:      "test-deployment-12",
+			}, &appsv1.Deployment{})
+		}, timeout, interval).ShouldNot(Succeed())
+	})
 })
