f5cecd846ebb01ba4da0b02df31a8efff8bddf35
Avital Tamir <avital.tamir@develeap.com>
2025-01-12 14:27:27 +0200

    Add e2e test

--- pkg/core/e2e/e2e_test.go
+++ pkg/core/e2e/e2e_test.go
@@ -358,4 +358,89 @@ var _ = Describe("Cyphernetes E2E", func() {
 			Expect(k8sClient.Delete(ctx, testDeployment)).Should(Succeed())
 		})
 	})
+
+	Context("Multiple Field Update Operations", func() {
+		It("Should update multiple fields in a deployment correctly", func() {
+			By("Creating test resources")
+			testDeployment := &appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-deployment-4",
+					Namespace: "default",
+					Labels: map[string]string{
+						"app": "test",
+					},
+				},
+				Spec: appsv1.DeploymentSpec{
+					Replicas: ptr.To(int32(1)),
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"app": "test",
+						},
+					},
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Labels: map[string]string{
+								"app": "test",
+							},
+						},
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "nginx",
+									Image: "nginx:1.19",
+								},
+							},
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, testDeployment)).Should(Succeed())
+
+			By("Executing a SET query to update multiple fields")
+			provider, err := apiserver.NewAPIServerProvider()
+			Expect(err).NotTo(HaveOccurred())
+
+			executor, err := core.NewQueryExecutor(provider)
+			Expect(err).NotTo(HaveOccurred())
+
+			ast, err := core.ParseQuery(`
+				MATCH (d:Deployment {name: "test-deployment-4"})
+				SET d.metadata.labels.environment = "production",
+					d.spec.replicas = 3
+				RETURN d
+			`)
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = executor.Execute(ast, "default")
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Verifying the updates in the cluster")
+			var updatedDeployment appsv1.Deployment
+
+			Eventually(func() int32 {
+				err := k8sClient.Get(ctx, client.ObjectKey{
+					Namespace: "default",
+					Name:      "test-deployment-4",
+				}, &updatedDeployment)
+				if err != nil {
+					return 0
+				}
+				return *updatedDeployment.Spec.Replicas
+			}, timeout*4, interval).Should(Equal(int32(3)))
+
+			Eventually(func() string {
+				err := k8sClient.Get(ctx, client.ObjectKey{
+					Namespace: "default",
+					Name:      "test-deployment-4",
+				}, &updatedDeployment)
+				if err != nil {
+					return ""
+				}
+				return updatedDeployment.Labels["environment"]
+			}, timeout*4, interval).Should(Equal("production"))
+
+			By("Cleaning up")
+			Expect(k8sClient.Delete(ctx, testDeployment)).Should(Succeed())
+		})
+	})
 })
