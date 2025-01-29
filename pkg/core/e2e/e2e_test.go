69fe7910ef2e0701f0baeafb8ccaf913153dddac
Avital Tamir <avital.tamir@develeap.com>
2025-01-29 05:34:09 +0200

    e2e aggregate on kindless

--- pkg/core/e2e/e2e_test.go
+++ pkg/core/e2e/e2e_test.go
@@ -17,6 +17,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/utils/pointer"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -35,6 +36,20 @@ var _ = Describe("Cyphernetes E2E", func() {
 
 	BeforeEach(func() {
 		ctx = context.Background()
+
+		// Clean up any leftover test resources
+		k8sClient.Delete(ctx, &appsv1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-deployment-agg-1",
+				Namespace: testNamespace,
+			},
+		})
+		k8sClient.Delete(ctx, &appsv1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-deployment-agg-2",
+				Namespace: testNamespace,
+			},
+		})
 	})
 
 	Context("Basic Query Operations", func() {
@@ -2103,6 +2118,163 @@ var _ = Describe("Cyphernetes E2E", func() {
 
 		By("Cleaning up")
 		Expect(k8sClient.Delete(ctx, testDeployment)).Should(Succeed())
+	})
+
+	It("Should handle aggregations with kindless nodes correctly", func() {
+		By("Creating test deployments with different replica counts")
+		// Create first deployment with 4 replicas
+		deployment1 := &appsv1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-deployment-agg-1",
+				Namespace: testNamespace, // Use testNamespace instead of "default"
+			},
+			Spec: appsv1.DeploymentSpec{
+				Replicas: pointer.Int32(4),
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"app": "test-deployment-agg-1",
+					},
+				},
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{
+							"app": "test-deployment-agg-1",
+						},
+					},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name:  "nginx",
+								Image: "nginx:latest",
+							},
+						},
+					},
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, deployment1)).Should(Succeed())
+
+		// Create second deployment with 2 replicas
+		deployment2 := &appsv1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-deployment-agg-2",
+				Namespace: testNamespace, // Use testNamespace instead of "default"
+			},
+			Spec: appsv1.DeploymentSpec{
+				Replicas: pointer.Int32(2),
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"app": "test-deployment-agg-2",
+					},
+				},
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{
+							"app": "test-deployment-agg-2",
+						},
+					},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name:  "nginx",
+								Image: "nginx:latest",
+							},
+						},
+					},
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, deployment2)).Should(Succeed())
+
+		// Wait for both deployments to complete their rollout
+		for _, deployName := range []string{"test-deployment-agg-1", "test-deployment-agg-2"} {
+			Eventually(func() bool {
+				var deployment appsv1.Deployment
+				err := k8sClient.Get(ctx, client.ObjectKey{
+					Namespace: testNamespace,
+					Name:      deployName,
+				}, &deployment)
+				if err != nil {
+					return false
+				}
+				return deployment.Status.ReadyReplicas == *deployment.Spec.Replicas &&
+					deployment.Status.Replicas == *deployment.Spec.Replicas
+			}, timeout*2, interval).Should(BeTrue(), fmt.Sprintf("Deployment %s should be ready", deployName))
+		}
+
+		// Wait for ReplicaSets to be created and have the correct number of replicas
+		Eventually(func() error {
+			rsList := &appsv1.ReplicaSetList{}
+			if err := k8sClient.List(ctx, rsList, client.InNamespace(testNamespace)); err != nil {
+				return err
+			}
+			if len(rsList.Items) < 2 {
+				return fmt.Errorf("waiting for ReplicaSets to be created, current count: %d", len(rsList.Items))
+			}
+			totalReplicas := 0
+			for _, rs := range rsList.Items {
+				if rs.Status.Replicas > 0 { // Only count active ReplicaSets
+					totalReplicas += int(rs.Status.Replicas)
+				}
+			}
+			if totalReplicas < 6 {
+				return fmt.Errorf("waiting for ReplicaSets to have correct replicas, current total: %d", totalReplicas)
+			}
+			return nil
+		}, timeout*2, interval).Should(Succeed())
+
+		// Wait for all pods to be created
+		Eventually(func() error {
+			pods := &corev1.PodList{}
+			if err := k8sClient.List(ctx, pods, client.InNamespace(testNamespace)); err != nil {
+				return err
+			}
+			if len(pods.Items) < 6 {
+				return fmt.Errorf("waiting for all pods to be created, current count: %d", len(pods.Items))
+			}
+			return nil
+		}, timeout*2, interval).Should(Succeed())
+
+		By("Executing query with kindless node and aggregation")
+		provider, err := apiserver.NewAPIServerProvider()
+		Expect(err).NotTo(HaveOccurred())
+
+		executor, err := core.NewQueryExecutor(provider)
+		Expect(err).NotTo(HaveOccurred())
+
+		query := `match (p:Pod)->(x)->(:Deployment) where x.spec.replicas > 1 return p.status.phase, sum{x.spec.replicas} as replicasSum`
+		ast, err := core.ParseQuery(query)
+		Expect(err).NotTo(HaveOccurred())
+
+		result, err := executor.Execute(ast, testNamespace)
+		Expect(err).NotTo(HaveOccurred())
+
+		By("Verifying the aggregation results")
+		// Check the pods array
+		pods, ok := result.Data["p"].([]interface{})
+		Expect(ok).To(BeTrue())
+		Expect(len(pods)).To(Equal(6)) // Should have 6 pods in total
+
+		// Check the replicaset array
+		replicaSets, ok := result.Data["x"].([]interface{})
+		Expect(ok).To(BeTrue())
+		Expect(len(replicaSets)).To(Equal(2)) // Should have 2 ReplicaSets
+
+		// Check the aggregate sum
+		aggregate, ok := result.Data["aggregate"].(map[string]interface{})
+		Expect(ok).To(BeTrue())
+		Expect(aggregate["replicasSum"]).To(Equal(float64(6))) // Total replicas should be 6 (4 + 2)
+
+		// Remove the pod phase check since we don't care about the running state
+		// Just verify we can access the phase
+		for _, pod := range pods {
+			podMap := pod.(map[string]interface{})
+			Expect(podMap["status"].(map[string]interface{})).To(HaveKey("phase"))
+		}
+
+		By("Cleaning up test resources")
+		Expect(k8sClient.Delete(ctx, deployment1)).Should(Succeed())
+		Expect(k8sClient.Delete(ctx, deployment2)).Should(Succeed())
 	})
 })
 
