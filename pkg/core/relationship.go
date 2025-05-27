269f2c204d2d397200d62180c9cdfda6995e0895
Anand Francis Joseph <anjoseph@redhat.com>
2025-05-27 21:39:08 +0530

    Added support for glob patterns in KindA
    Signed-off-by: Anand Francis Joseph <anjoseph@redhat.com>

--- pkg/core/relationship.go
+++ pkg/core/relationship.go
@@ -11,6 +11,7 @@ import (
 	"sync"
 
 	"github.com/avitaltamir/cyphernetes/pkg/provider"
+	"github.com/gobwas/glob"
 	"gopkg.in/yaml.v2"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -147,9 +148,6 @@ func InitializeRelationships(resourceSpecs map[string][]string, provider provide
 			kindsToCache[strings.ToLower(kindANameSingular)] = true
 		}
 	}
-
-	// Add Argo Application relationships
-	AddArgoRelationships(provider)
 
 	// Add existing rule kinds
 	for _, rule := range relationshipRules {
@@ -477,7 +475,7 @@ func InitializeRelationships(resourceSpecs map[string][]string, provider provide
 	}
 	potentialKindsMutex.Unlock()
 
-	customRelationshipsCount, err := loadCustomRelationships()
+	customRelationshipsCount, err := loadCustomRelationships(provider)
 	if err != nil && !CleanOutput {
 		fmt.Println("\nError loading custom relationships:", err)
 	}
@@ -517,7 +515,7 @@ func tryResolveGVR(provider provider.Provider, kind string) (schema.GroupVersion
 	return gvr, nil
 }
 
-func loadCustomRelationships() (int, error) {
+func loadCustomRelationships(provider provider.Provider) (int, error) {
 	counter := 0
 	// Get user's home directory
 	home, err := os.UserHomeDir()
@@ -568,6 +566,28 @@ func loadCustomRelationships() (int, error) {
 			}
 		}
 
+		// if there is any glob pattern in the rule's kind, evaluate the glob pattern and
+		// find matching resource kinds that is available in the cluster.
+		if strings.ContainsAny(rule.KindA, "*?[]{}") {
+			globA, err := glob.Compile(rule.KindA)
+			if err != nil {
+				return 0, err
+			}
+			for _, gvrName := range provider.GetKnownResourceKinds() {
+				if !globA.Match(strings.ToLower(gvrName)) {
+					continue
+				}
+				regexRule := RelationshipRule{
+					KindA:         strings.ToLower(gvrName),
+					KindB:         rule.KindB,
+					MatchCriteria: append([]MatchCriterion{}, rule.MatchCriteria...),
+					Relationship:  RelationshipType(fmt.Sprintf("%s_%s", rule.Relationship, strings.ToUpper(gvrName))),
+				}
+				counter++
+				relationshipRules = append(relationshipRules, regexRule)
+			}
+			continue
+		}
 		// Add to global relationships
 		counter++
 		AddRelationshipRule(rule)
@@ -896,25 +916,4 @@ func findExistingRelationshipRule(kindA, kindB string, gvrCache map[string]schem
 	}
 
 	return -1, false
-}
-
-func AddArgoRelationships(provider provider.Provider) {
-	for _, gvrName := range provider.GetKnownResourceKinds() {
-		if strings.ToLower(gvrName) == "bindings" || strings.Contains(strings.ToLower(gvrName), "review") {
-			continue
-		}
-		regexRule := RelationshipRule{
-			KindA: strings.ToLower(gvrName),
-			KindB: "applications",
-			MatchCriteria: []MatchCriterion{
-				{
-					FieldA:         "$.metadata.annotations.argocd\\.argoproj\\.io/tracking-id",
-					FieldB:         "$.metadata.name",
-					ComparisonType: StringContains,
-				},
-			},
-			Relationship: RelationshipType(fmt.Sprintf("%s_%s", "ARGO_APP", strings.ToUpper(gvrName))),
-		}
-		relationshipRules = append(relationshipRules, regexRule)
-	}
 }
